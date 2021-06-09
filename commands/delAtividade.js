8ed267b6bfd07cd8ccd28f8c06eb14d49ce8b251
Otávio Queiroz <otavioqs16@gmail.com>
2021-06-09 18:56:44 -0300

    update delAtividade command

--- commands/delAtividade.js
+++ commands/delAtividade.js
@@ -16,9 +16,16 @@ module.exports = {
         console.log(del)
 
         if(del === null){
-            embed.setTitle('Atividade não encontrada')
+            embed.setTitle('Atividade não encontrada!')
+            embed.setDescription(`**Tipo de atividade:** ${tipoAtividade} \n**Descrição:** ${desc}`)
+            embed.setColor('#2866c9')
+            embed.setTimestamp()
         }else{
+
             embed.setTitle('Atividade excluída com sucesso!')
+            embed.setDescription(`**Tipo de atividade:** ${tipoAtividade} \n**Descrição:** ${desc}`)
+            embed.setColor("#d60000")
+            embed.setTimestamp()
         }
 
         if(message){

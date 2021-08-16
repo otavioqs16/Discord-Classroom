4f2323cbf3f5246bcc97de0ff053a19fe847f042
Otávio Queiroz <otavioqs16@gmail.com>
2021-08-16 16:06:04 -0300

    update

--- commands/help.js
+++ commands/help.js
@@ -3,23 +3,27 @@ const Discord = require('discord.js')
 module.exports = {
     minArgs: 0,
     maxArgs: 0,
-    syntaxError: 'Incorrect syntax! Use `{PREFIX}`ping {ARGUMENTS}',
+    syntaxError: 'Incorrect syntax! Use `{PREFIX}`help',
     callback: async ({message, client}) => {
         let guild = client.guilds.cache.get("841406860556763187");
 
         const embed = new Discord.MessageEmbed()
         .setColor('#2866c9')
-        .setAuthor("Bot Teste", "https://imgur.com/aIut5rp.png")
+        .setAuthor("ClassRoom", "https://img.icons8.com/color/480/classroom.png")
         .setTitle(`Servidor: ${guild.name}`)
-        .setDescription('Comandos nesse servidor começam com ``!``')
+        .setDescription('Comandos nesse servidor começam com ``!`` ou ``/``')
         .addFields(
-            {name: '``!ping``', value: 'Testa conectividade com o bot e sua latência no momento'},
-            {name: '``!say``', value: 'Faz o bot escrever a mensagem a ser enviada'},
-            {name: '``!avatar``', value: 'Printa o avatar da pessoa mencionada ou o seu próprio avatar'},
-            {name: '``!help``', value: 'Mostra os comandos disponíveis do bot'},
-            {name: '``Boas-vindas``', value: 'Comando automático que mostra uma mensagem de boas-vindas quando alguém entra no servidor'},
-            {name: '``Kickado``', value: 'Comando automático que mostra uma mensagem quando alguém é expulso do servidor'}
-        ) 
+            {name: '``/addAtividade``', value: 'Adicionar nova atividade'},
+            {name: '``/addNota``', value: 'Adicionar nota de uma atividade a um determinado aluno'},
+            {name: '``/delAtividade``', value: 'Deletar uma atividade'},
+            {name: '``/listarTiposAtividade``', value: 'Listar todos os tipos de atividade existentes'},  
+            {name: '``/infoAtividade``', value: 'Informações de uma atividade específica'},
+            {name: '``!listarAtividades <tipo atividade>``', value: 'Lista todas as atividades de determinado tipo'},
+            {name: '``/listarTipo``', value: 'Listar todas as atividades relacionadas a um tipo de atividade'},
+            {name: '``!listarNotas <nome aluno>``', value: 'Lista as notas de determinado aluno'},
+            {name: '``!listarNotaAtividade <tipo atividade> <desc>``', value: 'Listar as notas dos alunos em determinada atividade'},
+            {name: '``/notaTotal``', value: 'Informa a nota total de determinado aluno'}
+            ) 
         await message.channel.send(embed);
     }
 }

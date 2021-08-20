13015eed7a1a58668eccaae114119b0a1c661cc2
Otávio Queiroz <otavioqs16@gmail.com>
2021-08-20 15:53:30 -0300

    update addNota command

--- commands/addNota.js
+++ commands/addNota.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js")
 const notas = require('../notas')
 const mongo = require('../mongo')
 const activitiesSchema = require('../activities-schema')
+const alunoSchema = require('../aluno-schema')
 
 module.exports = {
     slash: 'both',
@@ -20,44 +21,50 @@ module.exports = {
 
                 if(atividade.length <= 0){
                     embed.setTitle('ATIVIDADE NÃO ENCONTRADA')
+                    embed.setColor('#ffec5c')
                     embed.setDescription(`**Tipo de atividade:** ${tipoAtividade} \n**Descrição:** ${desc}`)
                     embed.setThumbnail('https://img.icons8.com/color/452/error--v1.png')
-                    embed.addField('Dica:', 'Utilize ``!listarAtividades <tipo da atividade>`` para checar as atividades existentes.')
+                    embed.addField('Dica:', 'Utilize ``/listarTiposAtividade`` para checar os tipos de atividades existentes.')
                     return embed
                 }
 
+                const ifAluno = await alunoSchema.find({nome})
+                console.log('ALUNO ENCONTRADO', ifAluno)
+                if(ifAluno.length <= 0){
+                    embed.setTitle('ALUNO NÃO ENCONTRADO')
+                    embed.setColor('#ffec5c')
+                    embed.setThumbnail('https://img.icons8.com/color/452/error--v1.png')
+                    embed.addField('Nome:', nome)
+                    embed.addField('Dica:', 'Utilize ``/listarAlunos`` para checar os alunos cadastrados.')
+                    return embed;
+                }
+
                 if(nota > atividade[0].nota){
-                    embed.setTitle('ESTÁ DANDO NOTA MAIOR DO QUE PODE')
-                    embed.setDescription(`NOTA ATUAl: ${nota} / NOTA MAXIMA: ${atividade[0].nota}`)
+                    embed.setTitle('NOTA EXCEDIDA')
+                    embed.setColor('#ffec5c')
+                    embed.setThumbnail('https://img.icons8.com/color/452/error--v1.png')
+                    embed.addField('Nota Concedida', nota)
+                    embed.addField('Nota Máxima da Atividade', atividade[0].nota) 
                     return embed
                 }
 
                 const addNota = await notas.addNota(nome, tipoAtividade, desc, nota)
                 console.log('ADICIONANDO NOTA A ATIVIDADE: ', addNota)
 
-                if(!addNota){
-                    embed.setTitle('ATIVIDADE NÃO ENCONTRADA')
-                    embed.setDescription(`**Tipo de atividade:** ${tipoAtividade} \n**Descrição:** ${desc}`)
-                    embed.setThumbnail('https://img.icons8.com/color/452/error--v1.png')
-                    embed.addField('Dica:', 'Utilize ``!listarAtividades <tipo da atividade>`` para checar as atividades existentes.')
+                embed.setTitle(tipoAtividade)
+                embed.setDescription(desc)
+                embed.setThumbnail('https://www.csvp.com.br/wp-content/uploads/2018/05/icone-prova.png')
+                embed.addField('Nome Aluno:', nome)
+                embed.addField('Nota:', nota + ' / ' + atividade[0].nota)
+                if((nota * 100) / atividade[0].nota >= 80){
+                    embed.setColor('#2ba61e')
+                }else if((nota * 100) / atividade[0].nota >= 60 & (nota * 100) / atividade[0].nota <= 80){
+                    embed.setColor('#0076ba')
+                }else{
+                    embed.setColor('#9e0000')
                 }
-                else{
-                    embed.setTitle(`Tipo de Atividade: ${tipoAtividade}`)
-                    embed.setDescription(`**Descrição:** ${desc}`)
-                    embed.setThumbnail('https://www.csvp.com.br/wp-content/uploads/2018/05/icone-prova.png')
-                    embed.addField('Nome Aluno:', nome)
-                    embed.addField('Nota:', nota + ' / ' + atividade[0].nota)
-                    if((nota * 100) / atividade[0].nota >= 80){
-                        embed.setColor('#2ba61e')
-                    }else if((nota * 100) / atividade[0].nota >= 60 & (nota * 100) / atividade[0].nota <= 80){
-                        embed.setColor('#0076ba')
-                    }else{
-                        embed.setColor('#9e0000')
-                    }
-                    embed.setFooter('Percentual atingido: ' + Math.round((nota * 100) / atividade[0].nota) + '%')
-                    
-                }
-        
+                embed.setFooter('Percentual atingido: ' + Math.round((nota * 100) / atividade[0].nota) + '%')
+
                 if(message) {
                     message.reply('', {embed})
                 }

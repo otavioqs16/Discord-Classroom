bc258bffd3fc55948a4af09eb3182eeee00ef1e4
Otávio Queiroz <otavioqs16@gmail.com>
2021-10-07 15:45:15 -0300

    update addNota command

--- commands/addNota.js
+++ commands/addNota.js
@@ -16,18 +16,6 @@ module.exports = {
         const [nome, tipoAtividade, desc, nota] = args;
         return await mongo().then(async mongoose => {
             try{
-                const atividade = await activitiesSchema.find({desc})
-                console.log('ATIVIDADE ENCONTRADA: ', atividade)
-
-                if(atividade.length <= 0){
-                    embed.setTitle('ATIVIDADE NÃO ENCONTRADA')
-                    embed.setColor('#ffec5c')
-                    embed.setDescription(`**Tipo de atividade:** ${tipoAtividade} \n**Descrição:** ${desc}`)
-                    embed.setThumbnail('https://img.icons8.com/color/452/error--v1.png')
-                    embed.addField('Dica:', 'Utilize ``/listarTiposAtividade`` para checar os tipos de atividades existentes.')
-                    return embed
-                }
-
                 const ifAluno = await alunoSchema.find({nome})
                 console.log('ALUNO ENCONTRADO', ifAluno)
                 if(ifAluno.length <= 0){
@@ -37,6 +25,17 @@ module.exports = {
                     embed.addField('Nome:', nome)
                     embed.addField('Dica:', 'Utilize ``/listarAlunos`` para checar os alunos cadastrados.')
                     return embed;
+                }
+
+                const atividade = await activitiesSchema.find({desc})
+                console.log('ATIVIDADE ENCONTRADA: ', atividade)
+                if(atividade.length <= 0){
+                    embed.setTitle('ATIVIDADE NÃO ENCONTRADA')
+                    embed.setColor('#ffec5c')
+                    embed.setDescription(`**Tipo de atividade:** ${tipoAtividade} \n**Descrição:** ${desc}`)
+                    embed.setThumbnail('https://img.icons8.com/color/452/error--v1.png')
+                    embed.addField('Dica:', 'Utilize ``/listarTiposAtividade`` para checar os tipos de atividades existentes.')
+                    return embed
                 }
 
                 if(nota > atividade[0].nota){
@@ -54,8 +53,8 @@ module.exports = {
                 embed.setTitle(tipoAtividade)
                 embed.setDescription(desc)
                 embed.setThumbnail('https://www.csvp.com.br/wp-content/uploads/2018/05/icone-prova.png')
-                embed.addField('Nome Aluno:', nome)
-                embed.addField('Nota:', nota + ' / ' + atividade[0].nota)
+                embed.addField('Nome Aluno', nome)
+                embed.addField('Nota', nota + ' / ' + atividade[0].nota)
                 if((nota * 100) / atividade[0].nota >= 80){
                     embed.setColor('#2ba61e')
                 }else if((nota * 100) / atividade[0].nota >= 60 & (nota * 100) / atividade[0].nota <= 80){

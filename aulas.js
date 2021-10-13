63c73e749592d9bf609b139b349ce7d994ebd522
Otávio Queiroz <otavioqs16@gmail.com>
2021-10-13 17:29:35 -0300

    create editAula and delAula functions

--- aulas.js
+++ aulas.js
@@ -24,3 +24,35 @@ module.exports.addAula = async (aula, tipo, link, data, hora) => {
         }
     })
 }
+
+module.exports.editAula = async (aula, novaAula, tipo, link, data, hora) => {
+    return await mongo().then(async mongoose => {
+        try{
+            const result = await aulaSchema.updateOne({
+                aula: aula
+            }, {
+                aula: novaAula,
+                tipo: tipo,
+                link: link,
+                data: data,
+                hora: hora
+            })
+            return result;
+        } finally{
+            mongoose.connection.close();
+        }
+    })
+}
+
+module.exports.delAula = async (aula) => {
+    return await mongo().then(async mongoose => {
+        try{
+            const result = await aulaSchema.findOneAndDelete({
+                aula: aula
+            })
+            return result;
+        } finally{
+            mongoose.connection.close();
+        }
+    })
+}
